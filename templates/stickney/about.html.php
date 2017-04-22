e2d2a28d488546f1f88286b97e540e375e04d316
remouk <remouk@gmail.com>
2017-04-22 21:38:13 +0200

    Update about.html.php

--- templates/stickney/about.html.php
+++ templates/stickney/about.html.php
@@ -15,7 +15,7 @@
 
 <p>
 	<?php /* Write whatever you want here, but please leave a link to phoboslab/asaph somewhere! */ ?>
-	This microblog is powered by <a href="http://www.phoboslab.org/projects/asaph">Asaph</a>.
+	This microblog is powered by <a href="https://github.com/remouk/Asaph-PHP7">Asaph-PHP7</a>.
 </p>
 
 </body>
